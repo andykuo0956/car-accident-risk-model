1e21eecd00ba7249ac72bc686d8ab06ac91dab33
andykuo0956 <andykuo0956@gmail.com>
2023-04-19 19:03:12 +0800

    init commit

---
+++ Flask-API-Risk-modelV1/risk_model_v1.py
@@ -0,0 +1,24 @@
+import pandas as py
+import geopandas as gpd
+from shapely.geometry import Point
+from shapely.ops import cascaded_union
+from shapely.geometry import Polygon
+
+def risk_model(polygon_appear,A1_traffic_accident_simple,A2_traffic_accident_simple):
+    total_risk = 0
+
+    for index in reversed(polygon_appear.index):
+        print(index)
+
+        A1_points_within_polygon = A1_traffic_accident_simple[A1_traffic_accident_simple.geometry.within(polygon_appear.iloc[index].geometry)]
+        print(A1_points_within_polygon)
+        A1_risk_sum = (A1_points_within_polygon['danger_val'].sum())*0.25
+
+        A2_points_within_polygon = A2_traffic_accident_simple[A2_traffic_accident_simple.geometry.within(polygon_appear.iloc[index].geometry)]
+        print(A2_points_within_polygon)
+        A2_risk_sum = (A2_points_within_polygon['danger_val'].sum())*0.25
+
+        total_risk += A1_risk_sum + A2_risk_sum
+    
+    return total_risk
+
---
+++ Flask-API-Risk-modelV1/run.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import numpy as np
+from risk_model_v1 import risk_model
+import geopandas as gpd
+
+app = Flask(__name__)
+CORS(app)
+
+A1_traffic_accident_simple = gpd.read_file(
+    "NTUDAC-Gogoro-Partnership-Project/car-accident-and-risk-model/rawData/traffic_accident_A1/A1_simple/A1_simple.shp")
+A2_traffic_accident_simple = gpd.read_file(
+    "NTUDAC-Gogoro-Partnership-Project/car-accident-and-risk-model/rawData/traffic_accident_A2/A2_simple/A2_simple.shp")
+
+@app.route('/calculate_risk', methods=['POST'])
+def calculate_risk():
+    print("---------1----------")
+    geojson_data = request.json
+    polygon_appear = gpd.GeoDataFrame.from_features(geojson_data)
+
+    total_risk = risk_model(
+        polygon_appear, A1_traffic_accident_simple, A2_traffic_accident_simple)
+
+    return jsonify({'total_risk': total_risk})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
